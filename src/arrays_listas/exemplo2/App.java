c7c371e8972f1050ea03a0c23f113865bcef2fbd
Paulo César Passos Pereira <paulo.passos1@gmail.com>
2023-05-03 08:37:42 -0300

    save

---
+++ src/arrays_listas/exemplo2/App.java
@@ -0,0 +1,34 @@
+package arrays_listas.exemplo2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantidade de produtos: ");
+        int n = sc.nextInt();
+
+        Product[] vetor = new Product[n]; 
+
+        for (int i = 0; i < n; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vetor[i] = new Product(name, price);
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += vetor[i].getPrice();
+        }
+
+        double avg = sum / n;
+        System.out.println("AVERAGE PRICE =  %.2f%n" +avg);
+
+        sc.close();
+    }
+    
+}
